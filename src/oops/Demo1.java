d9105975a6a9834c8d29093837f174d99370b042
Chinmayeetingne <rashitingne12@gmail.com>
2022-12-16 15:08:15 +0530

    program of method overiding

---
+++ src/oops/Demo1.java
@@ -0,0 +1,30 @@
+package oops;
+
+class A{
+    void showA()
+    {
+        System.out.println("This is show A");
+    }
+    void show()
+    {
+        System.out.println("I am in A");
+    }
+}
+class B extends A{
+    void showB()
+    {
+        System.out.println("This is show B");
+    }
+    void show()
+    {
+        System.out.println("I am in B");
+    }
+}
+class Demo1
+{
+    public static void main(String args[])
+    {
+        B b1 = new B();
+        b1.show();
+    }
+}
