c10f18ca698348f2f5ab6fdae907e508a5158459
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-13 10:52:18 +0530

    Scanner palindrome number

---
+++ src/com/bootcoding/java/scanner/ScannerPalindrome.java
@@ -0,0 +1,37 @@
+package com.bootcoding.java.scanner;
+
+import java.util.Scanner;
+
+public class ScannerPalindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Enter number " + (i + 1));
+            int number = sc.nextInt();
+            numbers[i] = number;
+        }
+
+        printPalindrome(numbers);
+    }
+
+    private static void printPalindrome(int[] numbers) {
+        System.out.println("Palindrome number ");
+            int temp = numbers[0];
+            for(int i=0;i<numbers.length;i++){
+                int rem = 0;
+                while(numbers[i]>0){
+                    int div = numbers[i] / 10;
+                    rem= rem *10 + numbers[i] % 10;
+                    numbers[i] = div;
+                }
+                if(rem == temp){
+                    System.out.println(temp);
+                }
+            }
+        }
+    }
+
