fd1ac3af02f2d38ca56b9a94c7560fc9245d88bd
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-16 17:06:09 +0530

    Level 2 assignment que 8

---
+++ src/com/bootcoding/java/level_2assignments/Four_Integers.java
@@ -0,0 +1,27 @@
+package com.bootcoding.java.level_2assignments;
+
+import oops.inheritance.DressingTable;
+
+import java.util.Scanner;
+
+public class Four_Integers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first integer : ");
+        int p = sc.nextInt();
+        System.out.println("Enter the second integer :");
+        int q = sc.nextInt();
+        System.out.println("Enter the third integer :");
+        int r = sc.nextInt();
+        System.out.println("Enter the fourth integer :");
+        int s= sc.nextInt();
+        if(r>0 && q>0 && (p%2==0) && q>r && s>p && r+s > p+q)
+        {
+            System.out.println("Correct values");
+        }
+        else
+            System.out.println("Wrong values");
+
+    }
+}
