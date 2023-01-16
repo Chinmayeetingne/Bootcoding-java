2044c81b556a57b6ba9695da5a48c8ae4b9bdeab
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-16 16:10:07 +0530

    Bike's Average consumption

---
+++ src/com/bootcoding/java/level_2assignments/AverageConsumption.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.level_2assignments;
+
+import java.util.Scanner;
+
+public class AverageConsumption {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total distance in km : ");
+        int distance = sc.nextInt();
+        System.out.println("Enter total fue spent in liters : ");
+        float fuel = sc.nextFloat();
+        float AverageConsumption =  distance / fuel;
+        System.out.println("Average Consumption(km/lt) : " +  AverageConsumption);
+
+    }
+}
