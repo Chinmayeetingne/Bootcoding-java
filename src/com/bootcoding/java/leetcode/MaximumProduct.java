1532972579c940687f58f80ec47e5c9a01b7791c
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-26 18:37:14 +0530

    Programs for maximum product of three numbers

--- src/com/bootcoding/java/leetcode/MaximumProduct.java
+++ src/com/bootcoding/java/leetcode/MaximumProduct.java
@@ -1,13 +1,17 @@
 package com.bootcoding.java.leetcode;
 
+import java.util.Arrays;
+
 public class MaximumProduct {
     public static void main(String[] args) {
-        int nums[] = {1,2,3};
-        int product = 0;
-         int max =0 , i=0;
+        int nums[] = {1,2,3,4};
+        System.out.println(maximumProduct(nums));
+    }
+    public static int maximumProduct(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
 
-
-
-         }
+        return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[n-1]*nums[0]*nums[1]);
+    }
 }
 
