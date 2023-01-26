5d4f5b642b3241093605e2c38217bf78d24f2a40
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-26 14:16:48 +0530

    Programs on array

---
+++ src/com/bootcoding/java/leetcode/MaximumProduct.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.leetcode;
+
+public class MaximumProduct {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3};
+        int product = 0;
+         int max =0 , i=0;
+
+
+
+         }
+}
+
