743894366e8a16398de9656d0d520dd97dfa3c6e
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-26 14:16:59 +0530

    Programs on array

--- src/com/bootcoding/java/scanner/MinMaxInArray.java
+++ src/com/bootcoding/java/scanner/MinMaxInArray.java
@@ -2,7 +2,7 @@ package com.bootcoding.java.scanner;
 
 import java.util.Scanner;
 
-public class ScannerMinMax {
+public class MinMaxInArray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
