29fc5851e28ca4acc8ed96e10d818fa0e1e38972
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-27 09:47:34 +0530

    Problems on patterns

--- src/com/bootcoding/java/Assigment/patterns/PatternWithNum.java
+++ src/com/bootcoding/java/Assigment/patterns/PatternWithNum.java
@@ -8,7 +8,7 @@ public class PatternWithNum {
         int rows = 5;
         for (int i = 1; i <= rows; i++)
         {
-            for (int j = 1; j <= i; j++)
+            for (int j = 1; j <=i; j++)
             {
                 System.out.print(i+" ");
             }
