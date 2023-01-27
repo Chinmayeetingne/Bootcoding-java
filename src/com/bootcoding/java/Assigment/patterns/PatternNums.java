b0f439bd56b99133c1ca7a5d369eed175721f7da
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-27 09:47:02 +0530

    Problems on patterns

--- src/com/bootcoding/java/Assigment/patterns/PatternNums.java
+++ src/com/bootcoding/java/Assigment/patterns/PatternNums.java
@@ -2,7 +2,7 @@ package com.bootcoding.java.Assigment.patterns;
 
 public class PatternNums {
     public static void main(String[] args) {
-        int  k = 1;
+        int  k = 1, n = 0;
         for(int i=0;i<=4;i++)
         {
             for(int j=1; j<i+1;j++)
