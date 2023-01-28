dabb25518c396aae1ca45cd65a96abd897b48650
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-28 08:19:25 +0530

    Leetcode problem

---
+++ src/com/bootcoding/java/leetcode/TwoSum.java
@@ -0,0 +1,23 @@
+package com.bootcoding.java.leetcode;
+
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        System.out.println(twoSum(nums ,9));
+}
+       public static int[] twoSum(int[] nums , int target){
+        HashMap<Integer,Integer> Index = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(Index.containsKey(target - nums[i])){
+                return new int[] {Index.get(target - nums[i]) ,i};
+            }
+            Index.put(nums[i] ,i);
+        }
+        return new int[] {};
+       }
+}
+
+
