3e88c44767a888b77b3576841a6ae3d078025b77
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-28 08:20:08 +0530

    Problem on strings

--- src/com/bootcoding/java/scanner/StringDemo.java
+++ src/com/bootcoding/java/scanner/StringDemo.java
@@ -2,7 +2,7 @@ package com.bootcoding.java.scanner;
 
 import java.util.Scanner;
 
-public class ScannerStringDemo {
+public class StringDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any word");
