6762583d5c6ca2ec64af2b6a9029659b05050690
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-28 08:17:41 +0530

    Problems on scanner

--- src/com/bootcoding/java/scanner/ScannerExample.java
+++ src/com/bootcoding/java/scanner/ScannerExample.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ScannerExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter number");
         int value = sc.nextInt();
         boolean res = isEven(value);
