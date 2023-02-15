663ba8484e74e61ff6ef49e7c5c475014baee3ab
Chinmayeetingne <rashitingne12@gmail.com>
2023-02-15 20:17:23 +0530

    Program for exception handling

---
+++ src/com/bootcoding/java/exceptionHandling/Example4.java
@@ -0,0 +1,22 @@
+package com.bootcoding.java.exceptionHandling;
+
+public class Example4 {
+    public static void main(String[] args) {
+        try {
+            int a[] = {2, 5, 7, 10, 12};
+            for (int i = 0; i < 5; i++) {
+                System.out.println(a[i]);
+            }
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("I am in finally block");
+        }
+        System.out.println("The end");
+    }
+}
