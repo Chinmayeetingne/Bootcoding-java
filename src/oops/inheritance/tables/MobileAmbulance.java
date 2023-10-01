7a46c41753b682222218ce0f87d7ef69dc45e392
Chinmayeetingne <rashitingne12@gmail.com>
2023-10-01 14:00:21 +0530

    Exercises

--- src/oops/inheritance/tables/MobileAmbulance.java
+++ src/oops/inheritance/tables/MobileAmbulance.java
@@ -1,6 +1,6 @@
-package oops.inheritance;
+package oops.inheritance.tables;
 
-import oops.encapsulation.Ambulance;
+import oops.encapsulation.ambulance.Ambulance;
 
 public class MobileAmbulance extends Ambulance {
     // parent protected property -> private to child class
